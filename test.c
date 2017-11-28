dab02c45390bf4d24d963a22a5195c1b57da1ee2
Basionkler <pergdr@yahoo.it>
2017-11-28 11:45:18 +0100

    backup

--- test.c
+++ test.c
@@ -1,11 +1,26 @@
 #include <CUnit/CUnit.h>
 #include "concurrent.h"
 
-buffer_t bufferUnitary;
-buffer_t buffer;
+buffer_t* bufferUnitary;
+buffer_t* buffer;
 
-/* SERVICE FUNCTIONS
+/* SERVICE FUNCTIONS */
 int init_createBuffer(void) {
     buffer = buffer_init(4);
     return buffer != NULL;
-}*/
+}
+
+int init_createBufferUnitary(void) {
+    bufferUnitary = buffer_init(1);
+    return bufferUnitary != NULL;
+}
+
+int clean_destroyBuffer(void) {
+    buffer_destroy(buffer);
+    return buffer == NULL;
+}
+
+int clean_destroyBufferUnitary(void) {
+    buffer_destroy(bufferUnitary);
+    return bufferUnitary == NULL;
+}
